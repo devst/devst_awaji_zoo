fff7ff2dcc4f74b3a76a4d750922c482da687528
Kamiya Atsuteru <teru.k.business@gmail.com>
2012-03-30 06:34:39 +0900

    疎通テスト

---
+++ src/main/java/features/sheep/Enquete.java
@@ -0,0 +1,15 @@
+package features.sheep;
+
+public class Enquete implements features.Enquete {
+
+	@Override
+	public Object getGoal() {
+		return "疎通テスト";
+	}
+
+	@Override
+	public Object getName() {
+		return "かみやさん";
+	}
+
+}
