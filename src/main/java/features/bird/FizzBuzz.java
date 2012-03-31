d1538dd0e4d4e316081c6fb866946422a1186b6b
Yu Ishida <distilledfields@gmail.com>
2012-03-31 15:07:39 +0900

    create a file.

---
+++ src/main/java/features/bird/FizzBuzz.java
@@ -0,0 +1,18 @@
+package features.bird;
+
+public class FizzBuzz implements features.FizzBuzz {
+
+	@Override
+	public String fizzBuzz(int number) {
+		if (number % 15 == 0) {
+			return "FizzBuzz";
+		} else if (number % 3 == 0) {
+			return "Fizz";
+		} else if (number % 5 == 0) {
+			return "Buzz";
+		} else {
+			return Integer.toString(number);
+		}
+	}
+
+}
