a0fde935a3b7a4da85abc1d600f0df7a20b2c3a9
Yu Ishida <distilledfields@gmail.com>
2012-03-31 14:17:19 +0900

    craete file

---
+++ src/main/java/features/bird/FirstPairWork.java
@@ -0,0 +1,15 @@
+package features.bird;
+
+public class FirstPairWork implements features.FirstPairWork {
+
+	@Override
+	public String getIkigomi() {
+		return "Hello TDD World!!!";
+	}
+
+	@Override
+	public String getMembers() {
+		return "eijit, ishiyu";
+	}
+
+}
