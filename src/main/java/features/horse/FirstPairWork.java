4525c2f0bdc21758f5527b68010eff4fcb6e2454
unknown <taka@coco-PC.(none)>
2012-03-31 14:18:54 +0900

    Modify and Add

---
+++ src/main/java/features/horse/FirstPairWork.java
@@ -0,0 +1,17 @@
+package features.horse;
+
+public class FirstPairWork implements features.FirstPairWork {
+
+	@Override
+	public String getIkigomi() {
+		// TODO Auto-generated method stub
+		return "‚ª‚ñ‚Î‚è‚Ü‚·";
+	}
+
+	@Override
+	public String getMembers() {
+		// TODO Auto-generated method stub
+		return "‚Î‚ñ‚Ç‚¤@‚¢‚Æ‚¤";
+	}
+
+}
