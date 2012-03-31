a6afbb70910d65f2cca38a4b8f04350b792be22d
Yoshihiro Furukawa <yoshihiro.furukawa@nifty.com>
2012-03-31 14:12:20 +0900

    Implimented FirstPairWork.java
    We implimented two methods, getIkigomi and getMembers.
    And modified pom.xml due to version of jar wad old.

---
+++ src/main/java/features/tiger/FiirstPairWork.java
@@ -0,0 +1,17 @@
+package features.tiger;
+
+import features.FirstPairWork;
+
+public class FiirstPairWork implements FirstPairWork {
+
+	@Override
+	public String getIkigomi() {
+		return "We will learn regarding TDD & Pair Programming.";
+	}
+
+	@Override
+	public String getMembers() {
+		return "naokirin and yfurukawa";
+	}
+
+}
