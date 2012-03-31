6893b67dd92e343782c45a6196b19fdd511e430b
pocketberserker <pocketberserker@gmail.com>
2012-03-31 15:56:43 +0900

    [from now] 2012/03/31 15:56:44

--- src/main/java/features/rabbit/FizzBuzz.java
+++ src/main/java/features/rabbit/FizzBuzz.java
@@ -3,9 +3,11 @@ package features.rabbit;
 public class FizzBuzz implements features.FizzBuzz {
 
 	@Override
-	public String fizzBuzz(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public String fizzBuzz(int num) {
+		if(num <= 0) throw new RuntimeException();
+		if(num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+		if(num % 5 == 0) return "Buzz";
+		if(num % 3 == 0) return "Fizz";
+		return String.valueOf(num);
 	}
-
 }
