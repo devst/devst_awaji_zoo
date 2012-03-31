87127baa219b409911c92d2a7af0da894f77c922
pocketberserker <pocketberserker@gmail.com>
2012-03-31 14:19:14 +0900

    FirstPairWorkを実装した

---
+++ src/main/java/features/rabbit/FirstPairWork.java
@@ -0,0 +1,15 @@
+package features.rabbit;
+
+public class FirstPairWork implements features.FirstPairWork {
+
+	@Override
+	public String getIkigomi() {
+		return "TDDを理解する！";
+	}
+
+	@Override
+	public String getMembers() {
+		return "white/pocketberserker";
+	}
+
+}
---
+++ src/main/java/features/rabbit/FirstPairWorkTest.java
@@ -0,0 +1,29 @@
+package features.rabbit;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import features.rabbit.FirstPairWork;
+
+public class FirstPairWorkTest {
+	
+	FirstPairWork sut;
+	
+	@Before
+	public void setUp() {
+		sut = new FirstPairWork();
+	}
+	
+	@Test
+	public void ペアの名前が取得できる() {
+		assertThat(sut.getMembers(), is("white/pocketberserker"));
+	}
+
+	@Test
+	public void 意気込みが取得できる() throws Exception {
+		assertThat(sut.getIkigomi(), is("TDDを理解する！"));
+	}
+}
