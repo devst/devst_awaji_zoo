20ded7f19336b6b42377e789be12beb3eb5c69d9
Kuchitama <kiyo.kunihira@gmail.com>
2012-03-31 14:17:30 +0900

    write my answer

---
+++ src/main/java/features/elephant/FirstPairWork.java
@@ -0,0 +1,15 @@
+package features.elephant;
+
+public class FirstPairWork implements features.FirstPairWork {
+
+	@Override
+	public String getIkigomi() {
+		return "いきごみ！";
+	}
+
+	@Override
+	public String getMembers() {
+		return "Momontyo & Kuchitama";
+	}
+
+}
