36650a3d4de2bf43ed874ae004b9d8699a4c60c9
Yu Ishida <distilledfields@gmail.com>
2012-03-31 15:09:33 +0900

    create a file.

---
+++ src/test/java/features/bird/FizzBuzzTest.java
@@ -0,0 +1,60 @@
+package features.bird;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import features.bird.FizzBuzz;
+
+public class FizzBuzzTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test1() {
+		this.testFizzBuzz(1, "1");
+	}
+	@Test
+	public void test2() {
+		this.testFizzBuzz(2, "2");
+	}
+	@Test
+	public void test3() {
+		this.testFizzBuzz(3, "Fizz");
+	}
+	@Test
+	public void test5() {
+		this.testFizzBuzz(5, "Buzz");
+	}
+	@Test
+	public void test6() {
+		this.testFizzBuzz(6, "Fizz");
+	}
+	@Test
+	public void test15() {
+		this.testFizzBuzz(15, "FizzBuzz");
+	}
+
+	private void testFizzBuzz(int num, String ans) {
+		FizzBuzz instance = new FizzBuzz();
+		assertEquals(ans, instance.fizzBuzz(num));
+ 	}
+}
