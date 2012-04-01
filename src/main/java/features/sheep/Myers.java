ef55bafcf53a300f20a71b852ba5be1b2d546cfc
tomo taka <tomo_taka01@hotmail.com>
2012-04-01 12:27:20 +0900

    三角形でない実装修正

--- src/main/java/features/sheep/Myers.java
+++ src/main/java/features/sheep/Myers.java
@@ -13,7 +13,9 @@ public class Myers implements features.Myers {
 
     @Override
     public String getName(int i, int i1, int i2) {
-        if (i <= 1 || i1 <= 1 || i2 <= 1){
+        if (i < 1 || i1 < 1 || i2 < 1){
+            throw new IllegalArgumentException();
+        } else if ((i >= i1+i2) || (i1 >= i+i2) || (i2 >= i+i1)){
             throw new IllegalArgumentException();
         } else if ((i == i1) && (i1 == i2)){
             return "正三角形";
