caad8d694aae04b5eec4839ad1d6f35fcac71943
pocketberserker <pocketberserker@gmail.com>
2012-04-02 04:31:12 +0900

    加算を行えるようにexecuteCalcを実装

---
+++ src/main/java/features/rabbit/Calculator.java
@@ -0,0 +1,20 @@
+package features.rabbit;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+public class Calculator implements features.Calculator {
+
+    private static final DecimalFormat RESULT_FORMAT = new DecimalFormat("#.###");
+
+    @Override
+    public String execute(String s) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public String executeCalc(String op ,String left, String right) {
+        BigDecimal leftNum = new BigDecimal(left);
+        BigDecimal rightNum = new BigDecimal(right);
+        return RESULT_FORMAT.format(leftNum.add(rightNum));
+    }
+}
---
+++ src/test/java/features/rabbit/CalculatorTest.java
@@ -0,0 +1,18 @@
+package features.rabbit;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Test;
+
+import features.rabbit.Calculator;
+
+public class CalculatorTest {
+
+    @Test
+	public void plusと2つの10進数値をexecuteCalculateに渡すと2つの数値の加算結果の文字列が返る() {
+    	
+        Calculator sut = new Calculator();
+        assertThat(sut.executeCalc("+", "5", "3"),is("8"));
+    }
+}
