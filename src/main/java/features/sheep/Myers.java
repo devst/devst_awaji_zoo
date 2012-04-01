189b3f12cf43e710f1e2d753e536580b49622c48
tomo taka <tomo_taka01@hotmail.com>
2012-04-01 10:52:05 +0900

    Myersの実装

---
+++ src/main/java/features/sheep/Myers.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package features.sheep;
+
+/**
+ *
+ * @author tomo
+ */
+
+public class Myers implements features.Myers {
+
+    @Override
+    public String getName(int i, int i1, int i2) {
+        if ((i == i1) && (i1 == i2)){
+            return "正三角形";
+        }else if ((i == i1) || (i1 == i2)){
+            return "二等辺三角形";
+        } else if (i <= 0 || i1 <= 0 || i2 <= 0){
+            throw new IllegalArgumentException();
+        } else {
+            return "不等辺三角形";
+        }
+        
+    }
+    
+}
