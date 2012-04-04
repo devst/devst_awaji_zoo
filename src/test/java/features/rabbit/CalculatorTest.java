61b893c7ef07873d97217b6a8f9a738e8bd5f3b7
pocketberserker <pocketberserker@gmail.com>
2012-04-05 00:06:42 +0900

    オブジェクトの生成をsetUpメソッドに抽出

--- src/test/java/features/rabbit/CalculatorTest.java
+++ src/test/java/features/rabbit/CalculatorTest.java
@@ -3,23 +3,29 @@ package features.rabbit;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import features.rabbit.Calculator;
 
 public class CalculatorTest {
+	
+	Calculator sut;
+	
+	@Before
+	public void setUp() {
+		sut = new Calculator();
+	}
 
     @Test
 	public void plusと2つの10進数値をexecuteCalculateに渡すと2つの数値の加算結果の文字列が返る() {
     	
-        Calculator sut = new Calculator();
         assertThat(sut.executeCalc("+", "5", "3"),is("8"));
     }
     
     @Test
     public void minusと2つの10進数値をexecuteCalculateに渡すと2つの数値の減算結果の文字列が返る() {
 
-        Calculator sut = new Calculator();
         assertThat(sut.executeCalc("-", "5", "3"),is("2"));
     }
 }
