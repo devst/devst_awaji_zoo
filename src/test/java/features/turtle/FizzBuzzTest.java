4a975a7b305eaa486f054dea4a4528493cb1b037
Cozykaz <cozykaz@gmail.com>
2012-04-08 00:55:14 +0900

    RemoveComplieError

--- src/test/java/features/turtle/FizzBuzzTest.java
+++ src/test/java/features/turtle/FizzBuzzTest.java
@@ -12,39 +12,18 @@ public class FizzBuzzTest {
 		assertEquals("Fizz", fb.fizzBuzz(3));
 	}
 	@Test
-<<<<<<< HEAD
-=======
-	public void FizzNegative() {
-		FizzBuzz fb = new FizzBuzz();
-		assertEquals("-1", fb.fizzBuzz(-1));
-	}
-	@Test
->>>>>>> 407b6341eeb8ba71b61388c3d17d99f1186151a0
 	public void Buzz() {
 		FizzBuzz fb = new FizzBuzz();
 		assertEquals("Buzz", fb.fizzBuzz(5));
-	}
-	@Test
-<<<<<<< HEAD
-	public void FizzBuzz() {
-		FizzBuzz fb = new FizzBuzz();
-		assertEquals("FizzBuzz", fb.fizzBuzz(15));
 	}
 	@Test(expected=RuntimeException.class)
 	public void FizzNegative() {
 		FizzBuzz fb = new FizzBuzz();
 		fb.fizzBuzz(-1);
 	}
-=======
-	public void BuzzNegative() {
-		FizzBuzz fb = new FizzBuzz();
-		assertEquals("-5", fb.fizzBuzz(-5));
-	}
 	@Test
 	public void FizzBuzz() {
 		FizzBuzz fb = new FizzBuzz();
 		assertEquals("FizzBuzz", fb.fizzBuzz(15));
 	}
-	
->>>>>>> 407b6341eeb8ba71b61388c3d17d99f1186151a0
 }
