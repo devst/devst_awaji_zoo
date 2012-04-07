c196cff696e48c8a82fd2b440ca41088fba1eea7
大野 恵正 <momontyo@oono-no-MacBook-Air.local>
2012-04-08 03:02:38 +0900

    除算の書式に渡すのをBigDecimalのままにした

--- src/main/java/features/elephant/Calculator.java
+++ src/main/java/features/elephant/Calculator.java
@@ -2,6 +2,7 @@ package features.elephant;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class Calculator implements features.Calculator {
@@ -30,9 +31,10 @@ public class Calculator implements features.Calculator {
 			return num1.multiply(num2).toString();
 		}else if(items[1].equals("/")){
 			DecimalFormat fmt = new DecimalFormat("0.###");
+			fmt.setRoundingMode(RoundingMode.HALF_UP);
 			BigDecimal num1a = new BigDecimal(items[0]);
 			BigDecimal num2a = new BigDecimal(items[2]);
-			return fmt.format(num1a.divide(num2a, 3, BigDecimal.ROUND_HALF_UP).doubleValue());
+			return fmt.format(num1a.divide(num2a, 3, BigDecimal.ROUND_HALF_UP));
 		}else{
 			throw new IllegalArgumentException(formula);
 		}
